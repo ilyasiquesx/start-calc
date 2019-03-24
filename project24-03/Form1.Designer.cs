996e7e11dad1122fa22e08001516dc819439f3f5
ilyasiquesx <ilyasiquesxhq@hotmail.com>
2019-03-24 17:45:20 +0300

    add

---
+++ project24-03/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace project24_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.digit7 = new System.Windows.Forms.Button();
+            this.digit8 = new System.Windows.Forms.Button();
+            this.digit9 = new System.Windows.Forms.Button();
+            this.digit4 = new System.Windows.Forms.Button();
+            this.digit5 = new System.Windows.Forms.Button();
+            this.digit6 = new System.Windows.Forms.Button();
+            this.digit1 = new System.Windows.Forms.Button();
+            this.digit2 = new System.Windows.Forms.Button();
+            this.digit3 = new System.Windows.Forms.Button();
+            this.signDiv = new System.Windows.Forms.Button();
+            this.signMulti = new System.Windows.Forms.Button();
+            this.signSub = new System.Windows.Forms.Button();
+            this.signPlus = new System.Windows.Forms.Button();
+            this.digit0 = new System.Windows.Forms.Button();
+            this.theDigit = new System.Windows.Forms.TextBox();
+            this.arithmeticSign = new System.Windows.Forms.TextBox();
+            this.subDigit = new System.Windows.Forms.TextBox();
+            this.signClear = new System.Windows.Forms.Button();
+            this.signEqual = new System.Windows.Forms.Button();
+            this.signDot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // digit7
+            // 
+            this.digit7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit7.Location = new System.Drawing.Point(11, 128);
+            this.digit7.Name = "digit7";
+            this.digit7.Size = new System.Drawing.Size(60, 60);
+            this.digit7.TabIndex = 0;
+            this.digit7.Text = "7";
+            this.digit7.UseVisualStyleBackColor = true;
+            this.digit7.Click += new System.EventHandler(this.digit7_Click);
+            // 
+            // digit8
+            // 
+            this.digit8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit8.Location = new System.Drawing.Point(77, 128);
+            this.digit8.Name = "digit8";
+            this.digit8.Size = new System.Drawing.Size(60, 60);
+            this.digit8.TabIndex = 1;
+            this.digit8.Text = "8";
+            this.digit8.UseVisualStyleBackColor = true;
+            this.digit8.Click += new System.EventHandler(this.digit8_Click);
+            // 
+            // digit9
+            // 
+            this.digit9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit9.Location = new System.Drawing.Point(143, 128);
+            this.digit9.Name = "digit9";
+            this.digit9.Size = new System.Drawing.Size(60, 60);
+            this.digit9.TabIndex = 2;
+            this.digit9.Text = "9";
+            this.digit9.UseVisualStyleBackColor = true;
+            this.digit9.Click += new System.EventHandler(this.digit9_Click);
+            // 
+            // digit4
+            // 
+            this.digit4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit4.Location = new System.Drawing.Point(11, 194);
+            this.digit4.Name = "digit4";
+            this.digit4.Size = new System.Drawing.Size(60, 60);
+            this.digit4.TabIndex = 3;
+            this.digit4.Text = "4";
+            this.digit4.UseVisualStyleBackColor = true;
+            this.digit4.Click += new System.EventHandler(this.digit4_Click);
+            // 
+            // digit5
+            // 
+            this.digit5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit5.Location = new System.Drawing.Point(77, 194);
+            this.digit5.Name = "digit5";
+            this.digit5.Size = new System.Drawing.Size(60, 60);
+            this.digit5.TabIndex = 4;
+            this.digit5.Text = "5";
+            this.digit5.UseVisualStyleBackColor = true;
+            this.digit5.Click += new System.EventHandler(this.digit5_Click);
+            // 
+            // digit6
+            // 
+            this.digit6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit6.Location = new System.Drawing.Point(143, 194);
+            this.digit6.Name = "digit6";
+            this.digit6.Size = new System.Drawing.Size(60, 60);
+            this.digit6.TabIndex = 5;
+            this.digit6.Text = "6";
+            this.digit6.UseVisualStyleBackColor = true;
+            this.digit6.Click += new System.EventHandler(this.digit6_Click);
+            // 
+            // digit1
+            // 
+            this.digit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit1.Location = new System.Drawing.Point(11, 260);
+            this.digit1.Name = "digit1";
+            this.digit1.Size = new System.Drawing.Size(60, 60);
+            this.digit1.TabIndex = 6;
+            this.digit1.Text = "1";
+            this.digit1.UseVisualStyleBackColor = true;
+            this.digit1.Click += new System.EventHandler(this.digit1_Click);
+            // 
+            // digit2
+            // 
+            this.digit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit2.Location = new System.Drawing.Point(77, 260);
+            this.digit2.Name = "digit2";
+            this.digit2.Size = new System.Drawing.Size(60, 60);
+            this.digit2.TabIndex = 7;
+            this.digit2.Text = "2";
+            this.digit2.UseVisualStyleBackColor = true;
+            this.digit2.Click += new System.EventHandler(this.digit2_Click);
+            // 
+            // digit3
+            // 
+            this.digit3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit3.Location = new System.Drawing.Point(143, 260);
+            this.digit3.Name = "digit3";
+            this.digit3.Size = new System.Drawing.Size(60, 60);
+            this.digit3.TabIndex = 8;
+            this.digit3.Text = "3";
+            this.digit3.UseVisualStyleBackColor = true;
+            this.digit3.Click += new System.EventHandler(this.digit3_Click);
+            // 
+            // signDiv
+            // 
+            this.signDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signDiv.Location = new System.Drawing.Point(209, 128);
+            this.signDiv.Name = "signDiv";
+            this.signDiv.Size = new System.Drawing.Size(60, 60);
+            this.signDiv.TabIndex = 9;
+            this.signDiv.Text = "÷";
+            this.signDiv.UseVisualStyleBackColor = true;
+            this.signDiv.Click += new System.EventHandler(this.signDiv_Click);
+            // 
+            // signMulti
+            // 
+            this.signMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signMulti.Location = new System.Drawing.Point(209, 194);
+            this.signMulti.Name = "signMulti";
+            this.signMulti.Size = new System.Drawing.Size(60, 60);
+            this.signMulti.TabIndex = 10;
+            this.signMulti.Text = "×";
+            this.signMulti.UseVisualStyleBackColor = true;
+            this.signMulti.Click += new System.EventHandler(this.signMulti_Click);
+            // 
+            // signSub
+            // 
+            this.signSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signSub.Location = new System.Drawing.Point(209, 260);
+            this.signSub.Name = "signSub";
+            this.signSub.Size = new System.Drawing.Size(60, 60);
+            this.signSub.TabIndex = 11;
+            this.signSub.Text = "-";
+            this.signSub.UseVisualStyleBackColor = true;
+            this.signSub.Click += new System.EventHandler(this.signSub_Click);
+            // 
+            // signPlus
+            // 
+            this.signPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signPlus.Location = new System.Drawing.Point(209, 326);
+            this.signPlus.Name = "signPlus";
+            this.signPlus.Size = new System.Drawing.Size(60, 60);
+            this.signPlus.TabIndex = 12;
+            this.signPlus.Text = "+";
+            this.signPlus.UseVisualStyleBackColor = true;
+            this.signPlus.Click += new System.EventHandler(this.signPlus_Click);
+            // 
+            // digit0
+            // 
+            this.digit0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.digit0.Location = new System.Drawing.Point(77, 326);
+            this.digit0.Name = "digit0";
+            this.digit0.Size = new System.Drawing.Size(60, 60);
+            this.digit0.TabIndex = 13;
+            this.digit0.Text = "0";
+            this.digit0.UseVisualStyleBackColor = true;
+            this.digit0.Click += new System.EventHandler(this.digit0_Click);
+            // 
+            // theDigit
+            // 
+            this.theDigit.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.theDigit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.theDigit.Location = new System.Drawing.Point(11, 84);
+            this.theDigit.Name = "theDigit";
+            this.theDigit.ReadOnly = true;
+            this.theDigit.Size = new System.Drawing.Size(324, 38);
+            this.theDigit.TabIndex = 14;
+            this.theDigit.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // arithmeticSign
+            // 
+            this.arithmeticSign.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.arithmeticSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.arithmeticSign.Location = new System.Drawing.Point(300, 47);
+            this.arithmeticSign.Name = "arithmeticSign";
+            this.arithmeticSign.ReadOnly = true;
+            this.arithmeticSign.Size = new System.Drawing.Size(35, 24);
+            this.arithmeticSign.TabIndex = 15;
+            this.arithmeticSign.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // subDigit
+            // 
+            this.subDigit.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.subDigit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.subDigit.Location = new System.Drawing.Point(11, 12);
+            this.subDigit.Name = "subDigit";
+            this.subDigit.ReadOnly = true;
+            this.subDigit.Size = new System.Drawing.Size(324, 22);
+            this.subDigit.TabIndex = 16;
+            this.subDigit.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // signClear
+            // 
+            this.signClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signClear.Location = new System.Drawing.Point(275, 128);
+            this.signClear.Name = "signClear";
+            this.signClear.Size = new System.Drawing.Size(60, 60);
+            this.signClear.TabIndex = 17;
+            this.signClear.Text = "C";
+            this.signClear.UseVisualStyleBackColor = true;
+            this.signClear.Click += new System.EventHandler(this.signClear_Click);
+            // 
+            // signEqual
+            // 
+            this.signEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signEqual.Location = new System.Drawing.Point(275, 194);
+            this.signEqual.Name = "signEqual";
+            this.signEqual.Size = new System.Drawing.Size(60, 60);
+            this.signEqual.TabIndex = 18;
+            this.signEqual.Text = "=";
+            this.signEqual.UseVisualStyleBackColor = true;
+            this.signEqual.Click += new System.EventHandler(this.signEqual_Click);
+            // 
+            // signDot
+            // 
+            this.signDot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signDot.Location = new System.Drawing.Point(143, 326);
+            this.signDot.Name = "signDot";
+            this.signDot.Size = new System.Drawing.Size(60, 60);
+            this.signDot.TabIndex = 19;
+            this.signDot.Text = ",";
+            this.signDot.UseVisualStyleBackColor = true;
+            this.signDot.Click += new System.EventHandler(this.signDot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 400);
+            this.Controls.Add(this.signDot);
+            this.Controls.Add(this.signEqual);
+            this.Controls.Add(this.signClear);
+            this.Controls.Add(this.subDigit);
+            this.Controls.Add(this.arithmeticSign);
+            this.Controls.Add(this.theDigit);
+            this.Controls.Add(this.digit0);
+            this.Controls.Add(this.signPlus);
+            this.Controls.Add(this.signSub);
+            this.Controls.Add(this.signMulti);
+            this.Controls.Add(this.signDiv);
+            this.Controls.Add(this.digit3);
+            this.Controls.Add(this.digit2);
+            this.Controls.Add(this.digit1);
+            this.Controls.Add(this.digit6);
+            this.Controls.Add(this.digit5);
+            this.Controls.Add(this.digit4);
+            this.Controls.Add(this.digit9);
+            this.Controls.Add(this.digit8);
+            this.Controls.Add(this.digit7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button digit7;
+        private System.Windows.Forms.Button digit8;
+        private System.Windows.Forms.Button digit9;
+        private System.Windows.Forms.Button digit4;
+        private System.Windows.Forms.Button digit5;
+        private System.Windows.Forms.Button digit6;
+        private System.Windows.Forms.Button digit1;
+        private System.Windows.Forms.Button digit2;
+        private System.Windows.Forms.Button digit3;
+        private System.Windows.Forms.Button signDiv;
+        private System.Windows.Forms.Button signMulti;
+        private System.Windows.Forms.Button signSub;
+        private System.Windows.Forms.Button signPlus;
+        private System.Windows.Forms.Button digit0;
+        private System.Windows.Forms.TextBox theDigit;
+        private System.Windows.Forms.TextBox arithmeticSign;
+        private System.Windows.Forms.TextBox subDigit;
+        private System.Windows.Forms.Button signClear;
+        private System.Windows.Forms.Button signEqual;
+        private System.Windows.Forms.Button signDot;
+    }
+}
+
---
+++ project24-03/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project24_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            theDigit.Text = 0.ToString();
+        }
+        public void isNullString()
+        {
+            if (theDigit.Text == "0")
+            {
+                theDigit.Text = null;
+            }
+        }
+        public void arithmeticEx()
+        {
+            subDigit.Text = null;
+            arithmeticSign.Text = null;
+            theDigit.Text = "Error";
+        }
+        private void digit0_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit0.Text;
+        }
+
+        private void digit1_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit1.Text;
+        }
+
+        private void digit2_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit2.Text;
+        }
+
+        private void digit3_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit3.Text;
+        }
+
+        private void digit4_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit4.Text;
+        }
+
+        private void digit5_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit5.Text;
+        }
+
+        private void digit6_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit6.Text;
+        }
+
+        private void digit7_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit7.Text;
+        }
+
+        private void digit8_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit8.Text;
+        }
+
+        private void digit9_Click(object sender, EventArgs e)
+        {
+            isNullString();
+            theDigit.Text += digit9.Text;
+        }
+        private void signDot_Click(object sender, EventArgs e)
+        {
+            if (theDigit.Text == "")
+            {
+                theDigit.Text += digit0.Text + signDot.Text;
+            }
+            if (!theDigit.Text.Contains(','))
+            {
+                theDigit.Text += signDot.Text;
+            }
+        }
+        private void signClear_Click(object sender, EventArgs e)
+        {
+            theDigit.Text = digit0.Text;
+            subDigit.Text = null;
+            arithmeticSign.Text = null;
+        }
+
+        private void signDiv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arithmeticSign.Text = signDiv.Text;
+                if (subDigit.Text != "" && theDigit.Text != "")
+                {
+                    subDigit.Text = (Convert.ToDouble(subDigit.Text) / Convert.ToDouble(theDigit.Text)).ToString();
+                    theDigit.Text = null;
+                }
+                else if (theDigit.Text != "")
+                {
+                    subDigit.Text = theDigit.Text;
+                    theDigit.Text = null;
+                }
+            }
+            catch (Exception)
+            {
+                arithmeticEx();
+            }
+        }
+        private void signMulti_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arithmeticSign.Text = signMulti.Text;
+                if (subDigit.Text != "" && theDigit.Text != "")
+                {
+                    subDigit.Text = (Convert.ToDouble(theDigit.Text) * Convert.ToDouble(subDigit.Text)).ToString();
+                    theDigit.Text = null;
+                }
+                else if (theDigit.Text != "")
+                {
+                    subDigit.Text = theDigit.Text;
+                    theDigit.Text = null;
+                }
+            }
+            catch
+            {
+                arithmeticEx();
+            }
+        }
+        private void signSub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arithmeticSign.Text = signSub.Text;
+                if (subDigit.Text != "" && theDigit.Text != "")
+                {
+                    subDigit.Text = (Convert.ToDouble(subDigit.Text) - Convert.ToDouble(theDigit.Text)).ToString();
+                    theDigit.Text = null;
+                }
+                else if (theDigit.Text != "")
+                {
+                    subDigit.Text = theDigit.Text;
+                    theDigit.Text = null;
+                }
+            }
+            catch
+            {
+                arithmeticEx();
+            }
+        }
+        private void signPlus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arithmeticSign.Text = signPlus.Text;
+                if (subDigit.Text != "" && theDigit.Text != "")
+                {
+                    subDigit.Text = (Convert.ToDouble(theDigit.Text) + Convert.ToDouble(subDigit.Text)).ToString();
+                    theDigit.Text = null;
+                }
+                else if (theDigit.Text != "")
+                {
+                    subDigit.Text = theDigit.Text;
+                    theDigit.Text = null;
+                }
+            }
+            catch
+            {
+                arithmeticEx();
+            }
+        }
+        private void signEqual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (arithmeticSign.Text == signMulti.Text && theDigit.Text != "")
+                {
+                    theDigit.Text = (Convert.ToDouble(theDigit.Text) * Convert.ToDouble(subDigit.Text)).ToString();
+                    subDigit.Text = null;
+                    arithmeticSign.Text = null;
+
+                }
+                if (arithmeticSign.Text == signSub.Text && theDigit.Text != "")
+                {
+                    theDigit.Text = (Convert.ToDouble(subDigit.Text) - Convert.ToDouble(theDigit.Text)).ToString();
+                    subDigit.Text = null;
+                    arithmeticSign.Text = null;
+                }
+                if (arithmeticSign.Text == signPlus.Text && theDigit.Text != "")
+                {
+                    theDigit.Text = (Convert.ToDouble(theDigit.Text) + Convert.ToDouble(subDigit.Text)).ToString();
+                    subDigit.Text = null;
+                    arithmeticSign.Text = null;
+                }
+                if (arithmeticSign.Text == signDiv.Text && theDigit.Text != "")
+                {
+                    theDigit.Text = (Convert.ToDouble(subDigit.Text) / Convert.ToDouble(theDigit.Text)).ToString();
+                    subDigit.Text = null;
+                    arithmeticSign.Text = null;
+                }
+                if (subDigit.Text != "" && theDigit.Text == "")
+                {
+                    theDigit.Text = subDigit.Text;
+                    subDigit.Text = null;
+                    arithmeticSign.Text = null;
+                }
+            }
+            catch
+            {
+                arithmeticEx();
+            }
+        }
+    }
+}
